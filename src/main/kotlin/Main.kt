d99c9d502e7d18930ad025b8e66359965cf67aa7
Romaric BANGA <romaric.banga@aninf.ga>
2021-03-21 16:27:01 +0100

    add component react

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,11 @@
 import kotlinx.browser.document
+import react.dom.h1
+import react.dom.render
 
 fun main() {
-    document.bgColor = "blue"
+    render(document.getElementById("root")) {
+        h1 {
+            +"Hello, React+Kotlin/JS!"
+        }
+    }
 }
